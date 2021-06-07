4b594b8dbcdfb950b4b4c139a380c17b8afcd1d0
shashika05 <shashikaraveenyasas@gmail.com>
2021-06-08 00:21:10 +0530

    home page fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -126,10 +126,10 @@ const Home = ({ history }) => {
           >
             <polygon points="50,0 100,0 50,100 0,100" />
           </svg>
-          <main className="mx-auto max-w-7xl px-4 sm:pt-24 sm:pb-8 sm:px-6 md:pt-16 lg:pt-20 lg:px-8 xl:pt-28 pt-16 pb-8">
+          <main className="mx-auto max-w-7xl px-4 sm:pt-24 sm:pb-8 sm:px-8 md:pt-16 md:pb-8 lg:pt-40 lg:px-12 xl:pt-36 pt-16 pb-12">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline animate-pulse cursor-pointer">
+                <span className="block animate-pulse cursor-pointer">
                   {greetings[greetSelector][0]}!
                 </span>
                 <br />
