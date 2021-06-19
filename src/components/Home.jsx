947faa7b05a5287850ac449f4e853bd076023e3e
shashika05 <shashikaraveenyasas@gmail.com>
2021-06-19 20:37:10 +0530

    spelling wrong

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -140,7 +140,14 @@ const Home = ({ history }) => {
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 I'm Shashika 'localhost443' Raveen who started his developer
-                carier by following JavaSctipt. Here you can learn lot of things
+                carier by following{" "}
+                <a
+                  href="https://en.wikipedia.org/wiki/JavaScript"
+                  target="_blank"
+                >
+                  JavaScript (JS)
+                </a>
+                . Here you can learn lot of things
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
